456048f4e89be1b42238629bafee5b7225ca4ef6
Unknown <chriswm@me.com>
2020-04-27 11:54:50 +0100

    Finished

---
+++ project/app/src/main/java/dev/chrismercer/dinnerpicker/MainActivity.kt
@@ -0,0 +1,30 @@
+package dev.chrismercer.dinnerpicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Burger King", "KFC", "Indian", "McDonalds", "Pizza")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener {
+            val randomNumber = Random()
+            val randomFood = randomNumber.nextInt(foodList.count()) //aleady 0 indexed
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
+    }
+
+
+}
